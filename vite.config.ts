11b8de25370ac8f5d6d6f1f80bd2af4bf8bbfcaf
typescreep <typescri@yandex.ru>
2025-05-23 07:34:48 +0300

    init

---
+++ vite.config.ts
@@ -0,0 +1,66 @@
+import path from 'path'
+import dotenv from 'dotenv'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
+
+const { VITE_BASEPREFIX } = process.env
+const { parsed: options } = await dotenv.config({ path: './.env.options' })
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  root: './',
+  base: VITE_BASEPREFIX || '/openapi-ui',
+  build: {
+    outDir: 'build',
+    modulePreload: false,
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false,
+  },
+  publicDir: 'public',
+  plugins: [
+    react(),
+    nodePolyfills({
+      include: ['buffer', 'process'],
+      globals: {
+        Buffer: true, // can also be 'build', 'dev', or false
+        global: true,
+        process: true,
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      api: path.resolve(__dirname, './src/api'),
+      components: path.resolve(__dirname, './src/components'),
+      constants: path.resolve(__dirname, './src/constants'),
+      localTypes: path.resolve(__dirname, './src/localTypes'),
+      mocks: path.resolve(__dirname, './src/mocks'),
+      pages: path.resolve(__dirname, './src/pages'),
+      store: path.resolve(__dirname, './src/store'),
+      templates: path.resolve(__dirname, './src/templates'),
+      utils: path.resolve(__dirname, './src/utils'),
+      hooks: path.resolve(__dirname, './src/hooks'),
+    },
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 4000,
+    proxy: {
+      '^/api/clusters/.*/k8s/': {
+        target: `${options?.KUBE_API_URL}/api/clusters`,
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+        rewrite: path => path.replace(/^\/api\/clusters\//, '/'),
+      },
+      '/clusterlist': {
+        target: `${options?.KUBE_API_URL}/clusterlist`,
+        changeOrigin: true,
+        secure: false,
+        rewrite: path => path.replace(/^\/clusterlist/, ''),
+      },
+    },
+  },
+})
