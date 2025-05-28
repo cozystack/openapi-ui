25e9d8841a918c187668963ae80ad583b16bee43
typescreep <typescri@yandex.ru>
2025-05-28 13:45:03 +0300

    factory improvement

--- src/components/organisms/Factory/Factory.tsx
+++ src/components/organisms/Factory/Factory.tsx
@@ -1,6 +1,32 @@
 import React, { FC } from 'react'
-import { DynamicExample } from '@prorobotech/openapi-k8s-toolkit'
+import { useParams } from 'react-router-dom'
+import {
+  DynamicComponents,
+  DynamicRendererWithProviders,
+  TDynamicComponentsAppTypeMap,
+  useDirectUnknownResource,
+  TFactoryResponse,
+} from '@prorobotech/openapi-k8s-toolkit'
+import { useSelector } from 'react-redux'
+import { RootState } from 'store/store'
+import { BASE_API_GROUP, BASE_API_VERSION } from 'constants/customizationApiGroupAndVersion'
 
 export const Factory: FC = () => {
-  return <DynamicExample />
+  const cluster = useSelector((state: RootState) => state.cluster.cluster)
+  const { key } = useParams()
+
+  const { data: factoryData } = useDirectUnknownResource<TFactoryResponse<TDynamicComponentsAppTypeMap>>({
+    uri: `/api/clusters/${cluster}/k8s/apis/${BASE_API_GROUP}/${BASE_API_VERSION}/factories/`,
+    refetchInterval: false,
+    queryKey: ['factories', cluster || 'no-cluster'],
+    isEnabled: cluster !== undefined,
+  })
+
+  const { spec } = factoryData?.items.find(({ spec }) => spec.key === key) ?? { spec: undefined }
+
+  if (!spec) {
+    return null
+  }
+
+  return <DynamicRendererWithProviders items={spec.data} components={DynamicComponents} />
 }
