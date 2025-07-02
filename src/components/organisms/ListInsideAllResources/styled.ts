16a26e94cba6ee013389afed861b4670d5b4eb27
typescreep <typescri@yandex.ru>
2025-07-02 14:43:38 +0300

    linters

--- src/components/organisms/ListInsideAllResources/styled.ts
+++ src/components/organisms/ListInsideAllResources/styled.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 const Grid = styled.div`
   display: grid;
   grid-gap: 72px;
+
   /* grid-template-columns: repeat(3, 1fr); */
   grid-template-columns: repeat(2, 1fr);
   height: 100%;
