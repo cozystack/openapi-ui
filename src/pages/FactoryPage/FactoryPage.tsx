f89fff6f1001de31146a99ac13dbebbbb389dc37
typescreep <typescri@yandex.ru>
2025-07-04 13:14:49 +0300

    edit id for forms | factory id

--- src/pages/FactoryPage/FactoryPage.tsx
+++ src/pages/FactoryPage/FactoryPage.tsx
@@ -1,5 +1,7 @@
 import React, { FC } from 'react'
+import { useParams } from 'react-router-dom'
 import { ManageableBreadcrumbs, Factory, NavigationContainer } from 'components'
+import { getBreadcrumbsIdPrefix } from 'utils/getBreadcrumbsIdPrefix'
 import { BaseTemplate } from 'templates'
 
 type TFactoryPageProps = {
@@ -7,10 +9,17 @@ type TFactoryPageProps = {
 }
 
 export const FactoryPage: FC<TFactoryPageProps> = ({ forcedTheme }) => {
+  const { namespace, syntheticProject, key } = useParams()
+
+  const breadcrumbsId = `${getBreadcrumbsIdPrefix({
+    instance: !!syntheticProject,
+    project: !!namespace,
+  })}factory-${key}`
+
   return (
     <BaseTemplate forcedTheme={forcedTheme} withNoCluster>
       <NavigationContainer>
-        <ManageableBreadcrumbs idToCompare={`factory-${window.location.pathname}`} />
+        <ManageableBreadcrumbs idToCompare={breadcrumbsId} />
       </NavigationContainer>
       <Factory />
     </BaseTemplate>
