6e3f5c11863f373306a0114d3503c5824342ccc7
typescreep <typescreep@prorobotech.ru>
2025-10-01 15:24:03 +0300

    search: design fixes

--- src/components/organisms/Search/styled.ts
+++ src/components/organisms/Search/styled.ts
@@ -13,7 +13,28 @@ const OverflowContainer = styled.div`
   scrollbar-width: thin;
 `
 
+type TEmptyContainerProps = {
+  $height?: number
+}
+
+const EmptyContainer = styled.div<TEmptyContainerProps>`
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  align-items: center;
+  height: ${({ $height }) => ($height ? `${$height}px` : '25vh')};
+`
+
+const EmptyText = styled.div`
+  margin-top: 12px;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 24px; /* 150% */
+`
+
 export const Styled = {
   Container,
   OverflowContainer,
+  EmptyContainer,
+  EmptyText,
 }
